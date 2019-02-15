b886f1380cb74c3b667fe23765996f360454ab50
Dennis Huillca Portilla <dhuillca@lan.wundermanphantasia.pe>
2019-02-15 17:54:52 -0500

    Add initial hello world react app with basic webpack and babel

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    mode: "development",
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loader", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        hot: true,
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
