fc9e1007ebc6f69bc62eb9137bd17fce74b95366
Dennis Huillca <dennisbot@gmail.com>
2019-02-19 18:52:37 -0500

    Add scss and fonts files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "../sass/style.scss";
 
 class App extends Component {
-    render () {
-        return <p>Hello from the App!</p>
-    }
+  render() {
+    return <div>
+    <h1>titulo h1</h1>
+    <p>Hello from the App!</p>
+    </div>;
+  }
 }
-export default App
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.scss$/,
+                test: /\.(css|scss)$/,
                 loaders: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
