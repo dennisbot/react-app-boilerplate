a1055459850606d0f05986a50c63191444c5d0be
Dennis Huillca <dennisbot@gmail.com>
2019-02-19 12:13:35 -0500

    Refactor webpack.config.js and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 var webpack = require("webpack");
 var path = require("path");
 
-var parentDir = path.join(__dirname, "../");
+var parentDir = path.join(__dirname, "");
 
 module.exports = {
     entry: [path.join(parentDir, "index.js")],
-    mode: "development",
     module: {
         rules: [
             {
@@ -14,8 +13,8 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /\.less$/,
-                loaders: ["style-loader", "css-loader", "less-loader"]
+                test: /\.scss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     },
