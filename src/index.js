6d5674f5da19c62beba31d004574e0f76b1a6b51
Dennis Huillca <dennis.huillca@wunderman.com>
2019-02-22 17:46:57 -0500

    Last update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
-
-render(<App />, document.getElementById('root'))
+console.log('mensaje de inicio, it works');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 var webpack = require("webpack");
 var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var sourceDirectory = path.join(__dirname, "src");
+// var data = require("json-loader!yaml-loader!./src/data/data.yaml");
+console.log("mensaje de prueba");
 
 module.exports = {
     entry: [path.join(sourceDirectory, "index.js")],
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+    },
     module: {
         rules: [
             {
@@ -15,13 +22,42 @@ module.exports = {
             {
                 test: /\.(css|scss)$/,
                 loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.pug$/,
+                include: [path.join(__dirname, "src", "layout")],
+                use: [
+                    {
+                        loader: "file-loader?name=[name].html"
+                    },
+                    {
+                        /**
+                         * We use extract-loader in this configuration because
+                         * file-loader above expects a string. In other configs,
+                         * we use HtmlWebpackLoader which handles the module
+                         * output of render-template-loader, so extract-loader
+                         * is not required.
+                         */
+                        loader: "extract-loader"
+                    },
+                    {
+                        loader: "render-template-loader",
+                        options: {
+                            engine: "pug",
+                            locals: {
+                                title: "Rendered with Pug!",
+                                desc: "Partials Support"
+                            },
+                            engineOptions: function(info) {
+                                return { filename: info.filename };
+                            }
+                        }
+                    }
+                ]
             }
         ]
     },
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: "bundle.js"
-    },
+    plugins: [],
     devServer: {
         hot: true,
         contentBase: sourceDirectory,
